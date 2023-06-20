c332ef57d789a7f0c211a2f5e99f45ff9f665012
shaharyaralvi <136804489+shaharyaralvi@users.noreply.github.com>
2023-06-21 00:18:38 +0800

    Add files via upload

---
+++ StudentRequestsAdd.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace APU_Programming_Café_Management_System.StudentForm
+{
+    partial class StudentRequestsAdd
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBoxLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBoxModule = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(69, 50);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(248, 25);
+            this.lblTitle.TabIndex = 54;
+            this.lblTitle.Text = "Write a Request to Lecturer";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnBack.Location = new System.Drawing.Point(566, 326);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 31);
+            this.btnBack.TabIndex = 53;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnWrite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWrite.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnWrite.Location = new System.Drawing.Point(656, 326);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 31);
+            this.btnWrite.TabIndex = 52;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = false;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(412, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Level :";
+            // 
+            // cmbBoxLevel
+            // 
+            this.cmbBoxLevel.FormattingEnabled = true;
+            this.cmbBoxLevel.Location = new System.Drawing.Point(415, 181);
+            this.cmbBoxLevel.Name = "cmbBoxLevel";
+            this.cmbBoxLevel.Size = new System.Drawing.Size(121, 24);
+            this.cmbBoxLevel.TabIndex = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(248, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Module :";
+            // 
+            // cmbBoxModule
+            // 
+            this.cmbBoxModule.FormattingEnabled = true;
+            this.cmbBoxModule.Location = new System.Drawing.Point(251, 181);
+            this.cmbBoxModule.Name = "cmbBoxModule";
+            this.cmbBoxModule.Size = new System.Drawing.Size(121, 24);
+            this.cmbBoxModule.TabIndex = 48;
+            // 
+            // StudentRequestsAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBoxLevel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBoxModule);
+            this.Name = "StudentRequestsAdd";
+            this.Size = new System.Drawing.Size(800, 406);
+            this.Load += new System.EventHandler(this.StudentRequestsAdd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBoxLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBoxModule;
+    }
+}
---
+++ StudentRequestsAdd.cs
@@ -0,0 +1,105 @@
+﻿using APU_Programming_Café_Management_System.Tables;
+using APU_Programming_Café_Management_System.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APU_Programming_Café_Management_System.StudentForm
+{
+    public partial class StudentRequestsAdd : UserControl
+    {
+        Student student;
+        StudentRequests studentRequests;
+        public StudentRequestsAdd(Student student, StudentRequests studentRequests)
+        {
+            InitializeComponent();
+            this.student = student;
+            this.studentRequests = studentRequests;
+
+            foreach (Row row in Programming_Café_DB.moduleTable.Rows)
+            {
+                cmbBoxModule.Items.Add(row.values[Programming_Café_DB.moduleTable.Name]);
+            }
+            cmbBoxLevel.Items.Add("Beginner");
+            cmbBoxLevel.Items.Add("Intermediate");
+            cmbBoxLevel.Items.Add("Advance");
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            Column columnToSearch = Programming_Café_DB.moduleTable.Name;
+            Column columnToReturn = Programming_Café_DB.moduleTable.Id;
+            string moduleId = Programming_Café_DB.moduleTable.Get_ColumnValue_From_Row(columnToSearch, cmbBoxModule.Text, columnToReturn);
+            List<string> requestValues = new List<string>
+            {
+                //first value is empty because its a primary key
+                "",
+                student.Id,
+                moduleId,
+                cmbBoxLevel.Text,
+            };
+
+
+            if (Check_If_Empty(requestValues))
+            {
+                MessageBox.Show("User section contains empty values");
+            }
+            else
+            {
+                bool ModulesAndLevelExists = false;
+                columnToSearch = Programming_Café_DB.studentModuleTable.StudentId;
+                List<Row> rows = Programming_Café_DB.studentModuleTable.Search_Row_For_Value(columnToSearch, student.Id);
+                foreach(Row row in rows)
+                {
+                    if (row.values[Programming_Café_DB.studentModuleTable.ModuleId] == moduleId && row.values[Programming_Café_DB.studentModuleTable.Level] == cmbBoxLevel.Text)
+                    {
+                        ModulesAndLevelExists = true;
+                        MessageBox.Show("Student is already enrolled into this module and level");
+                    }
+                }
+                if(!ModulesAndLevelExists)
+                {
+                    List<Column> uniqueColumns = new List<Column>()
+                    {
+                        Programming_Café_DB.requestTable.StudentId,
+                        Programming_Café_DB.requestTable.ModuleId,
+                        Programming_Café_DB.requestTable.Level
+                    };
+                    Programming_Café_DB.requestTable.Insert_Row(requestValues, uniqueColumns);
+                    studentRequests.Load_List_View();
+                    this.Dispose();
+                }
+
+            }
+        }
+        private bool Check_If_Empty(List<string> value)
+        {
+            bool is_Empty = false;
+            //starts at 1 because the first value is always empty
+            for (int i = 1; i < value.Count; i++)
+            {
+                if (value[i] == null || value[i] == "")
+                {
+                    is_Empty = true;
+                }
+            }
+            return is_Empty;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void StudentRequestsAdd_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
