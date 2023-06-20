6840b6275257429be099f0c9d63b10b16c2cf378
shaharyaralvi <136804489+shaharyaralvi@users.noreply.github.com>
2023-06-21 00:17:41 +0800

    Add files via upload

---
+++ StudentProfile.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace APU_Programming_Café_Management_System.StudentForm
+{
+    partial class StudentProfile
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxUsername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxTPNumber = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxPassword.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxPassword.Location = new System.Drawing.Point(140, 136);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxPassword.TabIndex = 51;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(137, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Password :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxUsername.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxUsername.Location = new System.Drawing.Point(140, 85);
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxUsername.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(137, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 16);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Username :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSave.Location = new System.Drawing.Point(589, 309);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 31);
+            this.btnSave.TabIndex = 47;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxEmail.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxEmail.Location = new System.Drawing.Point(359, 300);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxEmail.TabIndex = 46;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(356, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Email :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxPhone
+            // 
+            this.txtBoxPhone.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxPhone.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxPhone.Location = new System.Drawing.Point(359, 249);
+            this.txtBoxPhone.Name = "txtBoxPhone";
+            this.txtBoxPhone.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxPhone.TabIndex = 44;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(356, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "Phone :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxAddress
+            // 
+            this.txtBoxAddress.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxAddress.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxAddress.Location = new System.Drawing.Point(359, 192);
+            this.txtBoxAddress.Name = "txtBoxAddress";
+            this.txtBoxAddress.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxAddress.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(356, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Address :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxName.Location = new System.Drawing.Point(359, 85);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxName.TabIndex = 40;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(356, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Name :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxTPNumber
+            // 
+            this.txtBoxTPNumber.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxTPNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxTPNumber.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtBoxTPNumber.Location = new System.Drawing.Point(359, 136);
+            this.txtBoxTPNumber.Name = "txtBoxTPNumber";
+            this.txtBoxTPNumber.Size = new System.Drawing.Size(132, 22);
+            this.txtBoxTPNumber.TabIndex = 53;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(356, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 16);
+            this.label7.TabIndex = 52;
+            this.label7.Text = "TP Number :";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // StudentProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Controls.Add(this.txtBoxTPNumber);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBoxUsername);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentProfile";
+            this.Size = new System.Drawing.Size(800, 406);
+            this.Load += new System.EventHandler(this.StudentProfile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxUsername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxTPNumber;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ StudentProfile.cs
@@ -0,0 +1,62 @@
+﻿using APU_Programming_Café_Management_System.LecturerForm;
+using APU_Programming_Café_Management_System.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APU_Programming_Café_Management_System.StudentForm
+{
+    public partial class StudentProfile : UserControl
+    {
+        Student student;
+        public StudentProfile(Student student)
+        {
+            InitializeComponent();
+            this.student = student;
+        }
+
+        private void StudentProfile_Load(object sender, EventArgs e)
+        {
+            txtBoxUsername.Text = student.Username;
+            txtBoxPassword.Text = student.Password;
+            txtBoxName.Text = student.Name;
+            txtBoxTPNumber.Text = student.TP_Number;
+            txtBoxAddress.Text = student.Address;
+            txtBoxPhone.Text = student.Phone;
+            txtBoxEmail.Text = student.Email;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Column columnToSearch = Programming_Café_DB.userTable.Username;
+                int count = Programming_Café_DB.userTable.Search_Row_For_Value(columnToSearch, txtBoxUsername.Text).Count;
+                if (count == 0 || student.Username == txtBoxUsername.Text)
+                {
+                    student.Username = txtBoxUsername.Text;
+                    student.Password = txtBoxPassword.Text;
+                    student.Name = txtBoxName.Text;
+                    student.Address = txtBoxAddress.Text;
+                    student.Phone = txtBoxPhone.Text;
+                    student.Email = txtBoxEmail.Text;
+                    MessageBox.Show("Saved");
+                }
+                else
+                {
+                    MessageBox.Show("Username Already Exists");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Save Failed");
+            }
+        }
+    }
+}
